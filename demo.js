a9ed69f755563ba14a2aba5a3de2dbef86ca444e
imlawcs <daolehanhnguyen@gmail.com>
2024-05-20 12:46:39 +0700

    first commit

---
+++ demo.js
@@ -0,0 +1,60 @@
+//buoi4
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
+//import file json
+const users = require("./users.json");
+
+// Hàm ghi dữ liệu vào tệp JSON
+const saveUsersToFile = () => {
+  fs.writeFile('users.json', JSON.stringify(users, null, 2), (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+    }
+  });
+};
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get("/api/users/", (req, res) => {
+    // res.send(users);
+    res.send(users);
+})
+
+app.get("/api/users/:id/", (req, res) => {
+    const userId = parseInt(req.params.id);
+    res.send(users.filter(user => user.id === userId)); 
+    //dùng filter hoặc find
+}) 
+
+app.post("/api/users/" , (req, res) => {
+  const newUser = res.body;
+  newUser.id = users.length ? users[users.length - 1] + 1 : 1;
+  users.push(newUser);
+  saveUsersToFile();
+})
+
+app.put("/api/users/:id/", (req, res) => {
+  const userId = parseInt(req.params.id);
+  const user = users.find(user => user.id === userId);
+  Object.assign(user, req.body);
+  saveUsersToFile();
+})
+
+app.delete('/users/:id', (req, res) => {
+  const userIndex = users.findIndex(u => u.id === parseInt(req.params.id));
+  users.splice(userIndex, 1);
+  saveUsersToFile();
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
