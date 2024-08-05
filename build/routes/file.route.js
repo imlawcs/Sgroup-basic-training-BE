8903cfab1c69dd23f2b5ade9c9189ed33af73379
imlawcs <daolehanhnguyen@gmail.com>
2024-08-06 01:21:59 +0700

    test

--- build/routes/file.route.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const file = express.Router();
-const fileControllers = require('../controllers/file.controller.js');
-const authenticateToken = require('../middlewares/auth.middleware');
-file.post('/upload-file', authenticateToken.authenticateToken, fileControllers.uploadFile, fileControllers.check);
-module.exports = file;
